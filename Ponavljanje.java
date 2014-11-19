159c8ec58b7aa2f1d5f4ab794d713bbd8db09fbf
Gordan Sajević <gsajevic@hotmail.com>
2014-11-19 16:37:33 +0100

    ponavljanje

---
+++ Ponavljanje.java
@@ -0,0 +1,15 @@
+package lab8;
+
+public class Ponavljanje {
+
+	public static void main(String[] args) {
+		int brojac=0;
+		while (brojac<10)
+		{
+			System.out.println("Ja se mrzim ponavljati!");
+			brojac++;
+		}
+
+	}
+
+}
